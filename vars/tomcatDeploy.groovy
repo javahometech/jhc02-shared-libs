1ad782abd749d254135dc589183d79fdfadb976e
javahometech <kammana.hari@gmail.com>
2022-10-18 09:59:03 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,3 +1,8 @@
-def call(){
-  echo "tomcat deploy coming soon......"
+def call(creds,tomcatIp,userName,warName){
+  sshagent([creds]) {
+    // copy war file to tomcat webapps folder
+    sh "scp target/${warName}.war ${userName}@${tomcatIp}:/opt/tomcat9/webapps/"
+    sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/shutdown.sh"
+    sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/startup.sh"
+  }
 }
